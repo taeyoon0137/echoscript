04efe43c8151783d699b1760ff5ea232e8df6466
Taeyoon Lee <taeyoon0137@gmail.com>
2023-09-15 13:43:12 +0900

    Chore: Set up default environment

---
+++ @types/json.d.ts
@@ -0,0 +1,11 @@
+/**
+ * Copyright 2023 Taeyoon Lee. All Right Reserved.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+declare module '*.json' {
+  const json: object;
+  export default json;
+}
